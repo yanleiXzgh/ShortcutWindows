14f0b03c3f538022ad0d4cf8a39e69b6ec0c0545
yanleiXzgh <119163095+yanleiXzgh@users.noreply.github.com>
2024-02-19 15:23:53 +0800

    update

--- mopows4__incompleted.py
+++ mopows4__incompleted.py
@@ -14,9 +14,11 @@ class options():
     def new_App(self):    
         data = {}
         def pinput(resultp1,resultp2):
+            with open('data.json',mode='r') as f:
+                data = json.load(f)
             data[resultp1] = resultp2
             # data.append({'name': resultp1, 'address': resultp2})  # 将新数据添加到列表中
-            with open('data.json',mode='w') as  f:
+            with open('data.json',mode='w') as f:
                 json.dump(data,f)                           
         resultp1 = tks.askstring("store the app", "input the name：", parent=window_root)
         resultp2 = tks.askstring("store the app", "input the address：", parent=window_root)
